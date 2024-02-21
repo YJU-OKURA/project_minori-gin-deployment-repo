6f746195157ebc776bb6fa436107f93457bcda69
devYuMinKim <gimyumin40@gmail.com>
2024-02-21 15:31:50 +0900

    ♻️ refactor: Enhance main function logic for efficiency
    - Moved database initialization inside the RUN_MIGRATIONS check to optimize resource usage.
    - Extracted the router setup to a separate function for better modularity and readability.
    - Implemented error handling for server startup to catch potential issues during runtime.
    - Introduced configurable server port via environment variable for improved deployment flexibility.
    - Enhanced logging for critical operations such as database initialization and server startup.
    
    Related issue: #28

--- main.go
+++ main.go
@@ -5,8 +5,49 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"log"
 	"net/http"
+	"os"
 )
+
+func main() {
+	if shouldRunMigrations() {
+		db := migration.InitDB() // DB initialization
+		migration.Migrate(db)
+		os.Exit(0)
+	}
+
+	r := setupRouter()
+	if err := r.Run(getServerPort()); err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
+}
+
+func shouldRunMigrations() bool {
+	return os.Getenv("RUN_MIGRATIONS") == "true"
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/example")
+		{
+			eg.GET("/helloworld", Helloworld)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	return r
+}
+
+func getServerPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default port
+	}
+	return ":" + port
+}
 
 // @BasePath /api/v1
 
@@ -21,19 +62,4 @@ import (
 // @Router /example/helloworld [get]
 func Helloworld(g *gin.Context) {
 	g.JSON(http.StatusOK, "helloworld")
-}
-
-func main() {
-	//infra.Initialize()
-	r := gin.Default()
-	docs.SwaggerInfo.BasePath = "/api/v1"
-	v1 := r.Group("/api/v1")
-	{
-		eg := v1.Group("/example")
-		{
-			eg.GET("/helloworld", Helloworld)
-		}
-	}
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-	r.Run(":8080")
 }
