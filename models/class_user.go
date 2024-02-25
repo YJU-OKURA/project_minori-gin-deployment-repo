d84bcfb9ecb67d7c55886c91068a62b487840e1c
devYuMinKim <gimyumin40@gmail.com>
2024-02-26 01:04:08 +0900

    ♻️ refactor: Update ClassUser model with column tags and default for IsFavorite
    Enhanced the ClassUser model by specifying column tags for CID and UID, and a default value for IsFavorite.
    
    Related issue: #15

--- models/class_user.go
+++ models/class_user.go
@@ -1,11 +1,11 @@
 package models
 
 type ClassUser struct {
-	CID        uint   `gorm:"primaryKey"`
-	UID        uint   `gorm:"primaryKey"`
+	CID        uint   `gorm:"column:cid;primaryKey"`
+	UID        uint   `gorm:"column:uid;primaryKey"`
 	Nickname   string `gorm:"size:50;not null"`
-	IsFavorite bool
-	RoleID     int   `gorm:"not null"`
-	Class      Class `gorm:"foreignKey:CID"`
-	User       User  `gorm:"foreignKey:UID"`
+	IsFavorite bool   `gorm:"not null;default:false"`
+	RoleID     int    `gorm:"not null"`
+	Class      Class  `gorm:"foreignKey:CID"`
+	User       User   `gorm:"foreignKey:UID"`
 }
