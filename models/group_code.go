0373fb689024304ac249ca369225f824dcb86b98
devYuMinKim <gimyumin40@gmail.com>
2024-02-26 01:15:25 +0900

    ♻️ refactor: Update GroupCode model with nullable Secret and column annotations
    Modified the GroupCode model to make the Secret field a pointer, allowing null values. Also updated CID and UID fields with column annotations for clearer database mapping.
    
    Related issue: #39

--- models/group_code.go
+++ models/group_code.go
@@ -1,11 +1,11 @@
 package models
 
 type GroupCode struct {
-	ID     string `gorm:"primaryKey;size:255"`
-	Code   string `gorm:"size:10;not null"`
-	Secret string `gorm:"size:20"`
-	CID    uint   `gorm:"not null"` // Class ID
-	UID    uint   `gorm:"not null"` // User ID
-	Class  Class  `gorm:"foreignKey:CID"`
-	User   User   `gorm:"foreignKey:UID"`
+	ID     string  `gorm:"primaryKey;size:255"`
+	Code   string  `gorm:"size:10;not null"`
+	Secret *string `gorm:"size:20"`
+	CID    uint    `gorm:"column:cid;not null"` // Class ID
+	UID    uint    `gorm:"column:uid;not null"` // User ID
+	Class  Class   `gorm:"foreignKey:CID"`
+	User   User    `gorm:"foreignKey:UID"`
 }
