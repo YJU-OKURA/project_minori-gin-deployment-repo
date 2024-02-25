d48d03db082e274f7bf0eba8abcff0c704fa886f
devYuMinKim <gimyumin40@gmail.com>
2024-02-26 01:13:51 +0900

    ♻️ refactor: Update column annotations in GroupSchedule model
    Modified column annotations for CID in GroupSchedule model to include 'column' tag for clarity.
    
    Related issue: #15

--- models/group_schedule.go
+++ models/group_schedule.go
@@ -7,7 +7,7 @@ type GroupSchedule struct {
 	Title     string    `gorm:"size:255;not null"`
 	StartedAt time.Time `gorm:"not null"`
 	EndedAt   time.Time `gorm:"not null"`
-	CID       uint      `gorm:"not null"` // Class ID
+	CID       uint      `gorm:"column:cid;not null"` // Class ID
 	IsLive    bool      `gorm:"not null;default:false"`
 	Class     Class     `gorm:"foreignKey:CID"`
 }
