6b7ef8c9eb576e88c3e7dda777484faabe544864
devYuMinKim <gimyumin40@gmail.com>
2024-02-26 01:03:55 +0900

    ♻️ refactor: Add default value to Limitation in Class model
    Introduced a default value of 30 for the Limitation field in the Class model to ensure consistency.
    
    Related issue: #15

--- models/class.go
+++ models/class.go
@@ -1,9 +1,9 @@
 package models
 
 type Class struct {
-	ID          uint   `gorm:"primaryKey"`
-	Name        string `gorm:"size:30;not null"`
-	Limitation  *int
+	ID          uint    `gorm:"primaryKey"`
+	Name        string  `gorm:"size:30;not null"`
+	Limitation  *int    `gorm:"not null;default:30"`
 	Description *string `gorm:"size:255"`
 	Image       *string `gorm:"size:255"`
 }
