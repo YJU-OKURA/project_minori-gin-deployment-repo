2ab87b2f8ded5ca39faaffe8f89a92f01a6707cc
devYuMinKim <gimyumin40@gmail.com>
2024-02-26 20:27:31 +0900

    📝 docs: Update Swagger Documentation for Group Board API
    Updated Swagger documentation to include new routes and models for group board management in the API. This includes the addition of group board create, update, delete, and list endpoints with detailed descriptions and necessary parameters.
    
    Related issue:
    * #42
    * #9

--- docs/docs.go
+++ docs/docs.go
@@ -7,10 +7,10 @@ const docTemplate = `{
     "schemes": {{ marshal .Schemes }},
     "swagger": "2.0",
     "info": {
-        "description": "Minori API",
-        "title": "Minori API",
+        "description": "{{escape .Description}}",
+        "title": "{{.Title}}",
         "contact": {},
-        "version": "1.0.0"
+        "version": "{{.Version}}"
     },
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
@@ -38,8 +38,277 @@ const docTemplate = `{
                 }
             }
         },
-        "/gc/checkSecretExists": {
+        "/gb": {
+            "get": {
+                "description": "全てのグループ掲示板のリストを取得します。",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "group_board"
+                ],
+                "summary": "全てのグループ掲示板を取得",
+                "responses": {
+                    "200": {
+                        "description": "全てのグループ掲示板のリスト",
+                        "schema": {
+                            "type": "array",
+                            "items": {
+                                "type": "array",
+                                "items": {
+                                    "$ref": "#/definitions/models.GroupBoard"
+                                }
+                            }
+                        }
+                    },
+                    "500": {
+                        "description": "サーバーエラーが発生しました",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                }
+            },
             "post": {
+                "description": "Create a new group board with the provided information, including image upload.",
+                "consumes": [
+                    "multipart/form-data"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "group_board"
+                ],
+                "summary": "Create a new group board",
+                "parameters": [
+                    {
+                        "description": "Create group board",
+                        "name": "group_board_create",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/dto.GroupBoardCreateDTO"
+                        }
+                    },
+                    {
+                        "type": "string",
+                        "description": "Upload image file",
+                        "name": "image",
+                        "in": "formData"
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Group board created successfully",
+                        "schema": {
+                            "$ref": "#/definitions/models.GroupBoard"
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid request",
+                        "schema": {
+                            "type": "string"
+                        }
+                    },
+                    "500": {
+                        "description": "Server error",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        },
+        "/gb/announced": {
+            "get": {
+                "description": "公告されたグループ掲示板のリストを取得します。",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "group_board"
+                ],
+                "summary": "公告されたグループ掲示板を取得",
+                "responses": {
+                    "200": {
+                        "description": "公告されたグループ掲示板のリスト",
+                        "schema": {
+                            "type": "array",
+                            "items": {
+                                "type": "array",
+                                "items": {
+                                    "$ref": "#/definitions/models.GroupBoard"
+                                }
+                            }
+                        }
+                    },
+                    "500": {
+                        "description": "サーバーエラーが発生しました",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        },
+        "/gb/{id}": {
+            "get": {
+                "description": "指定されたIDのグループ掲示板の詳細を取得します。",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "group_board"
+                ],
+                "summary": "IDでグループ掲示板を取得",
+                "parameters": [
+                    {
+                        "type": "integer",
+                        "description": "グループ掲示板ID",
+                        "name": "id",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "グループ掲示板が取得されました",
+                        "schema": {
+                            "$ref": "#/definitions/models.GroupBoard"
+                        }
+                    },
+                    "400": {
+                        "description": "無効なリクエストです",
+                        "schema": {
+                            "type": "string"
+                        }
+                    },
+                    "404": {
+                        "description": "コードが見つかりません",
+                        "schema": {
+                            "type": "string"
+                        }
+                    },
+                    "500": {
+                        "description": "サーバーエラーが発生しました",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                }
+            },
+            "delete": {
+                "description": "指定されたIDのグループ掲示板を削除します。",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "group_board"
+                ],
+                "summary": "グループ掲示板を削除",
+                "parameters": [
+                    {
+                        "type": "integer",
+                        "description": "グループ掲示板ID",
+                        "name": "id",
+                        "in": "path",
+                        "required": true
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "グループ掲示板が正常に削除されました",
+                        "schema": {
+                            "type": "string"
+                        }
+                    },
+                    "404": {
+                        "description": "コードが見つかりません",
+                        "schema": {
+                            "type": "string"
+                        }
+                    },
+                    "500": {
+                        "description": "サーバーエラーが発生しました",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                }
+            },
+            "patch": {
+                "description": "指定されたIDのグループ掲示板の詳細を更新します。",
+                "consumes": [
+                    "application/json"
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "group_board"
+                ],
+                "summary": "グループ掲示板を更新",
+                "parameters": [
+                    {
+                        "type": "integer",
+                        "description": "グループ掲示板ID",
+                        "name": "id",
+                        "in": "path",
+                        "required": true
+                    },
+                    {
+                        "description": "グループ掲示板の更新",
+                        "name": "group_board_update",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/dto.GroupBoardUpdateDTO"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "グループ掲示板が正常に更新されました",
+                        "schema": {
+                            "$ref": "#/definitions/models.GroupBoard"
+                        }
+                    },
+                    "400": {
+                        "description": "リクエストが不正です",
+                        "schema": {
+                            "type": "string"
+                        }
+                    },
+                    "404": {
+                        "description": "コードが見つかりません",
+                        "schema": {
+                            "type": "string"
+                        }
+                    },
+                    "500": {
+                        "description": "サーバーエラーが発生しました",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        },
+        "/gc/checkSecretExists": {
+            "get": {
                 "description": "指定されたグループコードにシークレットがあるかどうかをチェックする。",
                 "consumes": [
                     "application/json"
@@ -53,13 +322,11 @@ const docTemplate = `{
                 "summary": "グループコードにシークレットが存在するかチェック",
                 "parameters": [
                     {
-                        "description": "Check Secret",
-                        "name": "group_code_check_request",
-                        "in": "body",
-                        "required": true,
-                        "schema": {
-                            "$ref": "#/definitions/dto.GroupCodeCheckRequest"
-                        }
+                        "type": "string",
+                        "description": "Code to check",
+                        "name": "code",
+                        "in": "query",
+                        "required": true
                     }
                 ],
                 "responses": {
@@ -70,13 +337,13 @@ const docTemplate = `{
                         }
                     },
                     "400": {
-                        "description": "error\" \"無効なリクエストです",
+                        "description": "無効なリクエストです",
                         "schema": {
                             "type": "string"
                         }
                     },
                     "404": {
-                        "description": "error\" \"コードが見つかりません",
+                        "description": "コードが見つかりません",
                         "schema": {
                             "type": "string"
                         }
@@ -85,7 +352,7 @@ const docTemplate = `{
             }
         },
         "/gc/verifyGroupCode": {
-            "post": {
+            "get": {
                 "description": "グループコードと、該当する場合はそのシークレットを確認する。",
                 "consumes": [
                     "application/json"
@@ -99,36 +366,41 @@ const docTemplate = `{
                 "summary": "グループコードとシークレットを検証",
                 "parameters": [
                     {
-                        "description": "グループコードの確認",
-                        "name": "group_code_request",
-                        "in": "body",
-                        "required": true,
-                        "schema": {
-                            "$ref": "#/definitions/dto.GroupCodeRequest"
-                        }
+                        "type": "string",
+                        "description": "Code to verify",
+                        "name": "code",
+                        "in": "query",
+                        "required": true
+                    },
+                    {
+                        "type": "string",
+                        "description": "Secret for the code",
+                        "name": "secret",
+                        "in": "query",
+                        "required": true
                     }
                 ],
                 "responses": {
                     "200": {
-                        "description": "message\" \"グループコードが検証されました",
+                        "description": "グループコードが検証されました",
                         "schema": {
                             "type": "string"
                         }
                     },
                     "400": {
-                        "description": "error\" \"無効なリクエストです",
+                        "description": "無効なリクエストです",
                         "schema": {
                             "type": "string"
                         }
                     },
                     "401": {
-                        "description": "error\" \"シークレットが一致しません",
+                        "description": "シークレットが一致しません",
                         "schema": {
                             "type": "string"
                         }
                     },
                     "404": {
-                        "description": "error\" \"コードが見つかりません",
+                        "description": "コードが見つかりません",
                         "schema": {
                             "type": "string"
                         }
@@ -138,27 +410,137 @@ const docTemplate = `{
         }
     },
     "definitions": {
-        "dto.GroupCodeCheckRequest": {
+        "dto.GroupBoardCreateDTO": {
             "type": "object",
             "required": [
-                "code"
+                "cid",
+                "content",
+                "title",
+                "uid"
             ],
             "properties": {
-                "code": {
+                "cid": {
+                    "type": "integer"
+                },
+                "content": {
+                    "type": "string",
+                    "example": "Sample Content"
+                },
+                "image": {
+                    "type": "string"
+                },
+                "is_announced": {
+                    "type": "boolean",
+                    "default": false
+                },
+                "title": {
+                    "type": "string",
+                    "example": "Sample Title"
+                },
+                "uid": {
+                    "type": "integer"
+                }
+            }
+        },
+        "dto.GroupBoardUpdateDTO": {
+            "type": "object",
+            "required": [
+                "id"
+            ],
+            "properties": {
+                "content": {
+                    "type": "string"
+                },
+                "id": {
+                    "type": "integer"
+                },
+                "image": {
+                    "type": "string"
+                },
+                "is_announced": {
+                    "type": "boolean"
+                },
+                "title": {
                     "type": "string"
                 }
             }
         },
-        "dto.GroupCodeRequest": {
+        "models.Class": {
             "type": "object",
-            "required": [
-                "code"
-            ],
             "properties": {
-                "code": {
+                "description": {
                     "type": "string"
                 },
-                "secret": {
+                "id": {
+                    "type": "integer"
+                },
+                "image": {
+                    "type": "string"
+                },
+                "limitation": {
+                    "type": "integer"
+                },
+                "name": {
+                    "type": "string"
+                }
+            }
+        },
+        "models.GroupBoard": {
+            "type": "object",
+            "properties": {
+                "cid": {
+                    "description": "Class ID",
+                    "type": "integer"
+                },
+                "class": {
+                    "$ref": "#/definitions/models.Class"
+                },
+                "content": {
+                    "type": "string"
+                },
+                "createdAt": {
+                    "type": "string"
+                },
+                "id": {
+                    "type": "integer"
+                },
+                "image": {
+                    "type": "string"
+                },
+                "isAnnounced": {
+                    "type": "boolean"
+                },
+                "title": {
+                    "type": "string"
+                },
+                "uid": {
+                    "description": "User ID",
+                    "type": "integer"
+                },
+                "updatedAt": {
+                    "type": "string"
+                },
+                "user": {
+                    "$ref": "#/definitions/models.User"
+                }
+            }
+        },
+        "models.User": {
+            "type": "object",
+            "properties": {
+                "createdAt": {
+                    "type": "string"
+                },
+                "email": {
+                    "type": "string"
+                },
+                "id": {
+                    "type": "integer"
+                },
+                "image": {
+                    "type": "string"
+                },
+                "name": {
                     "type": "string"
                 }
             }
