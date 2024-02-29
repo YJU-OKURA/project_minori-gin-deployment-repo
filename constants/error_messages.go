2a56002bbceb1fa800f1d8d3cd22b75109fe655b
devYuMinKim <gimyumin40@gmail.com>
2024-02-29 23:09:46 +0900

    ♻️ refactor: Organize error messages and HTTP status codes
    - Error messages in 'error_messages.go' have been restructured for better clarity and separation into categories: client errors, authentication errors, and server & database errors. Additionally, new error messages for file size and file type have been added.
    
    - In 'http_status_codes.go', the HTTP status codes have been expanded to include a wider range of client and server error responses, such as Forbidden, Conflict, and Gateway Timeout.
    
    - This restructuring enhances code readability and makes error handling more precise.
    
    Related issue: #34

--- constants/error_messages.go
+++ constants/error_messages.go
@@ -1,22 +1,39 @@
 package constants
 
+// クライアントエラー関連のエラーメッセージ
 const (
-	InvalidRequest           = "無効なリクエストです"       // 400 Bad Request
-	BadRequestMessage        = "リクエストが不正です"       // 400 Bad Request
-	CodeNotFound             = "コードが見つかりません"      // 404 Not Found
-	Unauthorized             = "認証に失敗しました"        // 401 Unauthorized
-	SecretMismatch           = "シークレットが一致しません"    // 401 Unauthorized
-	InternalServerError      = "サーバーエラーが発生しました"   // 500 Internal Server Error
-	DatabaseError            = "データベースエラーが発生しました" // 500 Internal Server Error
-	UnknownError             = "不明なエラーが発生しました"    // 500 Internal Server Error
-	Success                  = "成功"               // 200 OK
-	GroupCodeExists          = "グループコードが存在します"    // 200 OK
-	SecretExists             = "シークレットが存在します"     // 200 OK
-	GroupCodeVerified        = "グループコードが検証されました"  // 200 OK
-	ErrNoFileHeaderJP        = "ファイルヘッダが提供されていません"
-	ErrOpenFileJP            = "ファイルのオープンに失敗しました"
-	ErrReadFileDataJP        = "ファイルデータの読み取りに失敗しました"
-	ErrLoadAWSConfigJP       = "AWS設定のロードに失敗しました"
-	ErrUploadToS3JP          = "S3へのアップロードに失敗しました"
-	ErrCloudFrontURLNotSetJP = "AWS_CLOUDFRONT環境変数が設定されていません"
+	InvalidRequest    = "無効なリクエストです"          // 400 Bad Request
+	BadRequestMessage = "リクエストが不正です"          // 400 Bad Request
+	ErrNoFileHeaderJP = "ファイルヘッダが提供されていません"   // 400 Bad Request
+	ErrFileSizeJP     = "ファイルサイズが10MBを超えています" // 400 Bad Request
+	ErrMimeTypeJP     = "ファイルタイプが画像ではありません"   // 400 Bad Request
+)
+
+// 認証関連のエラーメッセージ
+const (
+	Unauthorized   = "認証に失敗しました"     // 401 Unauthorized
+	SecretMismatch = "シークレットが一致しません" // 401 Unauthorized
+	CodeNotFound   = "コードが見つかりません"   // 404 Not Found
+)
+
+// サーバーエラー&データベース関連のエラーメッセージ
+const (
+	InternalServerError      = "サーバーエラーが発生しました"               // 500 Internal Server Error
+	DatabaseError            = "データベースエラーが発生しました"             // 500 Internal Server Error
+	UnknownError             = "不明なエラーが発生しました"                // 500 Internal Server Error
+	ErrOpenFileJP            = "ファイルのオープンに失敗しました"             // 500 Internal Server Error
+	ErrReadFileDataJP        = "ファイルデータの読み取りに失敗しました"          // 500 Internal Server Error
+	ErrLoadAWSConfigJP       = "AWS設定のロードに失敗しました"             // 500 Internal Server Error
+	ErrUploadToS3JP          = "S3へのアップロードに失敗しました"            // 500 Internal Server Error
+	ErrCloudFrontURLNotSetJP = "AWS_CLOUDFRONT環境変数が設定されていません" // 500 Internal Server Error
+	AssignError              = "ロールの割り当てに失敗しました"              // 500 Internal Server Error
+)
+
+// 成功時のメッセージ
+const (
+	Success                 = "成功"                 // 200 OK
+	GroupCodeExists         = "グループコードが存在します"      // 200 OK
+	SecretExists            = "シークレットが存在します"       // 200 OK
+	GroupCodeVerified       = "グループコードが検証されました"    // 200 OK
+	ClassMemberRegistration = "グループコードの確認と役割の割り当て" // 200 OK
 )
--- constants/http_status_codes.go
+++ constants/http_status_codes.go
@@ -1,10 +1,29 @@
 package constants
 
-// HTTP status codes
 const (
-	StatusOK                  = 200 // OK
-	StatusBadRequest          = 400 // Bad Request
-	StatusUnauthorized        = 401 // Unauthorized
-	StatusNotFound            = 404 // Not Found
+	/*
+		成功時のステータスコード
+	*/
+	StatusOK       = 200 // OK
+	StatusCreated  = 201 // Created
+	StatusAccepted = 202 // Accepted
+
+	/*
+		クライアントエラー ステータスコード
+	*/
+	StatusBadRequest       = 400 // Bad Request
+	StatusUnauthorized     = 401 // Unauthorized
+	StatusForbidden        = 403 // Forbidden
+	StatusNotFound         = 404 // Not Found
+	StatusMethodNotAllowed = 405 // Method Not Allowed
+	StatusConflict         = 409 // Conflict
+
+	/*
+		サーバーエラー ステータスコード
+	*/
 	StatusInternalServerError = 500 // Internal Server Error
+	StatusNotImplemented      = 501 // Not Implemented
+	StatusBadGateway          = 502 // Bad Gateway
+	StatusServiceUnavailable  = 503 // Service Unavailable
+	StatusGatewayTimeout      = 504 // Gateway Timeout
 )
