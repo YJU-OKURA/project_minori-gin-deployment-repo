bfc69d8af278bc496a661f15e79d6706b4f52b3f
devYuMinKim <gimyumin40@gmail.com>
2024-03-03 03:27:01 +0900

    ✨ feat: Enhance AWS uploader utility
    Enhanced uploader.go to improve the AWS S3 uploading process with better error handling and file naming.
    
    Related issue: #42

--- utils/uploader.go
+++ utils/uploader.go
@@ -7,15 +7,20 @@ import (
 	"github.com/YJU-OKURA/project_minori-gin-deployment-repo/constants"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"io"
+	"log"
 	"mime/multipart"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 type Uploader interface {
-	UploadImage(file *multipart.FileHeader) (string, error)
+	UploadImage(file *multipart.FileHeader, classID uint, isLogo bool) (string, error)
 }
 
 type awsUploader struct {
@@ -27,24 +32,49 @@ func NewAwsUploader() Uploader {
 
 // initializeS3Client S3クライアントを初期化
 func initializeS3Client() (*s3.Client, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	awsRegion := os.Getenv("AWS_REGION")
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(awsRegion),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+			os.Getenv("AWS_S3_ACCESS_KEY"),
+			os.Getenv("AWS_S3_SECRET_ACCESS_KEY"),
+			"",
+		)),
+	)
 	if err != nil {
-		return nil, fmt.Errorf("%s: %w", constants.DatabaseError, err) // Using constants for error message
+		return nil, fmt.Errorf("%s: %w", constants.DatabaseError, err)
 	}
 	return s3.NewFromConfig(cfg), nil
 }
 
 // UploadImage 画像をアップロード
-func (u *awsUploader) UploadImage(fileHeader *multipart.FileHeader) (string, error) {
+func (u *awsUploader) UploadImage(fileHeader *multipart.FileHeader, classID uint, isLogo bool) (string, error) {
+	log.Printf("UploadImage called with classID: %d, isLogo: %t", classID, isLogo)
 	if fileHeader == nil {
 		return "", fmt.Errorf(constants.ErrNoFileHeaderJP)
+	}
+
+	const maxSize = 10 << 20 // 10MB
+	if fileHeader.Size > maxSize {
+		return "", fmt.Errorf(constants.ErrFileSizeJP)
+	}
+
+	mimeType := fileHeader.Header.Get("Content-Type")
+	if !strings.HasPrefix(mimeType, "image/") {
+		return "", fmt.Errorf("%s：%s", constants.ErrMimeTypeJP, mimeType)
 	}
 
 	file, err := fileHeader.Open()
 	if err != nil {
 		return "", fmt.Errorf("%s: %w", constants.ErrOpenFileJP, err)
 	}
-	defer file.Close()
+	defer func() {
+		if cerr := file.Close(); cerr != nil && err == nil {
+			log.Printf("Failed to close file: %v", cerr)
+			err = cerr
+		}
+	}()
 
 	fileData, err := io.ReadAll(file)
 	if err != nil {
@@ -57,14 +87,32 @@ func (u *awsUploader) UploadImage(fileHeader *multipart.FileHeader) (string, err
 	}
 	uploader := manager.NewUploader(s3Client)
 
+	// ファイル名の生成
+	extension := filepath.Ext(fileHeader.Filename)
+	uniqueFileName := fmt.Sprintf("images/%d/%s-%d%s", classID, strings.TrimSuffix(fileHeader.Filename, extension), time.Now().Unix(), extension)
+	//log.Printf("Generated uniqueFileName: %s", uniqueFileName)
+
+	// ロゴの場合、パスに 'logo/' を追加
+	if isLogo {
+		uniqueFileName = fmt.Sprintf("images/%d/logo/%s-%d%s", classID, strings.TrimSuffix(fileHeader.Filename, extension), time.Now().Unix(), extension)
+	} else {
+		uniqueFileName = fmt.Sprintf("images/%d/%s-%d%s", classID, strings.TrimSuffix(fileHeader.Filename, extension), time.Now().Unix(), extension)
+	}
+
+	bucketName := os.Getenv("AWS_S3_BUCKET_NAME")
+	if bucketName == "" {
+		return "", fmt.Errorf(constants.ErrLoadAWSConfigJP)
+	}
+
 	upParams := &s3.PutObjectInput{
-		Bucket: aws.String("images"), // Assuming bucket name is 'images'
-		Key:    aws.String(fileHeader.Filename),
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(uniqueFileName),
 		Body:   bytes.NewReader(fileData),
 	}
 
 	_, err = uploader.Upload(context.TODO(), upParams)
 	if err != nil {
+		log.Printf("Error in uploader.Upload: %v", err)
 		return "", fmt.Errorf("%s: %w", constants.ErrUploadToS3JP, err)
 	}
 
@@ -73,6 +121,7 @@ func (u *awsUploader) UploadImage(fileHeader *multipart.FileHeader) (string, err
 		return "", fmt.Errorf(constants.ErrCloudFrontURLNotSetJP)
 	}
 
-	finalURL := fmt.Sprintf("%s/%s", cloudFrontURL, fileHeader.Filename) // Ensure the URL is correctly formatted
+	finalURL := fmt.Sprintf("%s/%s", cloudFrontURL, uniqueFileName)
+	log.Printf("Final URL: %s", finalURL)
 	return finalURL, nil
 }
