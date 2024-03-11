e4991684d2a6b81958b14d0fd51ee5b5be1890e7
Regulus0811 <kbc6268@g.yju.ac.kr>
2024-03-11 22:29:41 +0900

    ✨ feat: Add new method for refresh token in GoogleAuthService
    Implemented a new method in GoogleAuthService to handle refresh tokens, enhancing the authentication mechanism. This addition aims to provide more robust and secure authentication by facilitating token renewal.
    
    Related issue: #32

--- services/google_auth_service.go
+++ services/google_auth_service.go
@@ -7,8 +7,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/YJU-OKURA/project_minori-gin-deployment-repo/dto"
+	"github.com/YJU-OKURA/project_minori-gin-deployment-repo/models"
+	"github.com/YJU-OKURA/project_minori-gin-deployment-repo/repositories"
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -16,14 +21,43 @@ import (
 // GoogleAuthServiceはGoogle認証サービスのインターフェース
 type GoogleAuthService interface {
 	GenerateStateOauthCookie(w http.ResponseWriter) string
+	GenerateToken(userID uint) (string, error)
+	GenerateRefreshToken(userID uint) (string, error)
 	GetGoogleUserInfo(code string) ([]byte, error)
 	OauthConfig() *oauth2.Config
+	UpdateOrCreateUser(userInput dto.UserInput) (models.User, error)
+	GetUserByID(userID uint) (models.User, error)
+	ValidateToken(tokenString string) (*jwt.Token, error)
 }
 
 // GoogleAuthServiceImplはGoogle認証サービスの実装
 type GoogleAuthServiceImpl struct {
 	oauthConfig *oauth2.Config
 	UrlAPI      string
+	repo        repositories.GoogleAuthRepository
+}
+
+func (s *GoogleAuthServiceImpl) GetUserByID(id uint) (models.User, error) {
+	return s.repo.GetUserByID(id)
+}
+
+func (s *GoogleAuthServiceImpl) ValidateToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("tkq!mwe!d#s"), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func (s *GoogleAuthServiceImpl) UpdateOrCreateUser(userInput dto.UserInput) (models.User, error) {
+	return s.repo.UpdateOrCreateUser(userInput)
 }
 
 // OauthConfigはOAuth設定を返す
@@ -31,17 +65,49 @@ func (s *GoogleAuthServiceImpl) OauthConfig() *oauth2.Config {
 	return s.oauthConfig
 }
 
+func (s *GoogleAuthServiceImpl) GenerateToken(userID uint) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user_id"] = userID
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func (s *GoogleAuthServiceImpl) GenerateRefreshToken(userID uint) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user_id"] = userID
+	claims["type"] = "refresh"
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
 // NewGoogleAuthServiceはGoogle認証サービスの新しいインスタンスを作成
-func NewGoogleAuthService() GoogleAuthService {
+func NewGoogleAuthService(repo repositories.GoogleAuthRepository) GoogleAuthService {
 	return &GoogleAuthServiceImpl{
 		oauthConfig: &oauth2.Config{
-			RedirectURL:  "http://localhost:8080/auth/google/callback",
-			ClientID:     "500381013046-96fkfveskcksbd77a833qobg48887jcd.apps.googleusercontent.com",
-			ClientSecret: "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",
+			RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+			ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+			ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
 			Scopes:       []string{"https://www.googleapis.com/auth/userinfo.profile"},
 			Endpoint:     google.Endpoint,
 		},
 		UrlAPI: "https://www.googleapis.com/oauth2/v2/userinfo?access_token=",
+		repo:   repo,
 	}
 }
 
@@ -70,4 +136,15 @@ func (s *GoogleAuthServiceImpl) GetGoogleUserInfo(code string) ([]byte, error) {
 	}
 
 	return ioutil.ReadAll(resp.Body)
+}
+
+func (s *GoogleAuthServiceImpl) RefreshAccessToken(refreshToken string) (*oauth2.Token, error) {
+	tokenSource := s.oauthConfig.TokenSource(context.Background(), &oauth2.Token{
+		RefreshToken: refreshToken,
+	})
+	newToken, err := tokenSource.Token()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to refresh access token: %v", err)
+	}
+	return newToken, nil
 }
