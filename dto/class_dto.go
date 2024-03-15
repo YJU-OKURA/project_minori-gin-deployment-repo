de059f2becc76d2bbeeac188e5f2454e97d17ebe
Regulus0811 <kbc6268@g.yju.ac.kr>
2024-03-15 09:03:28 +0900

    ♻️ Refactor: Modify comments in the code to Japanese for better understanding
    This commit involves the modification of existing comments within the codebase, translating them into Japanese. The purpose behind this change is to improve the understandability and accessibility of the code for Japanese developers, aiming to foster a clearer and more efficient development process.
    
    Related issue:#88

--- dto/class_dto.go
+++ dto/class_dto.go
@@ -2,7 +2,7 @@ package dto
 
 // CreateClassRequest クラス作成リクエストDTO
 type CreateClassRequest struct {
-	Name        string  `form:"name"`        // 클래스 이름
-	Limitation  *int    `form:"limitation"`  // 참여 인원 제한
-	Description *string `form:"description"` // 클래스 설명
+	Name        string  `form:"name"`        // クラス名
+	Limitation  *int    `form:"limitation"`  // 参加制限人数
+	Description *string `form:"description"` // クラス説明
 }
