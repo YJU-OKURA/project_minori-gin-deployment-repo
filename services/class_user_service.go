f59469a9a45fa1f61fa289c13580ca6d6fb7609f
Regulus0811 <kbc6268@g.yju.ac.kr>
2024-03-16 18:43:12 +0900

    ✨ feat: Implement Search User in class user service
    Implemented a robust user search functionality within the ClassUser service, enabling precise and efficient user searches within classes. This feature leverages advanced querying techniques to filter and return user data based on specific search criteria. The enhancement facilitates a more dynamic and flexible user management experience, allowing for quick identification and access to user profiles within any class context. This addition significantly streamlines administrative tasks and improves the overall functionality and user-friendliness of the class management system.
    
    Related issue:#90

--- services/class_user_service.go
+++ services/class_user_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/YJU-OKURA/project_minori-gin-deployment-repo/dto"
 	"github.com/YJU-OKURA/project_minori-gin-deployment-repo/models"
 	"github.com/YJU-OKURA/project_minori-gin-deployment-repo/repositories"
 )
@@ -10,6 +11,7 @@ type ClassUserService interface {
 	AssignRole(uid uint, cid uint, roleName string) error
 	GetRole(uid uint, cid uint) (int, error)
 	UpdateUserName(uid uint, cid uint, newName string) error
+	GetUserClasses(uid uint) ([]dto.UserClassInfoDTO, error)
 }
 
 // classUserServiceImpl はClassCodeServiceの実装です。
@@ -19,7 +21,10 @@ type classUserServiceImpl struct {
 }
 
 func NewClassUserService(classUserRepo repositories.ClassUserRepository, roleRepo repositories.RoleRepository) ClassUserService {
-	return &classUserServiceImpl{classUserRepo: classUserRepo, roleRepo: roleRepo}
+	return &classUserServiceImpl{
+		classUserRepo: classUserRepo,
+		roleRepo:      roleRepo,
+	}
 }
 
 func (s *classUserServiceImpl) AssignRole(uid uint, cid uint, roleName string) error {
@@ -43,4 +48,8 @@ func (s *classUserServiceImpl) GetRole(uid uint, cid uint) (int, error) {
 
 func (s *classUserServiceImpl) UpdateUserName(uid uint, cid uint, newName string) error {
 	return s.classUserRepo.UpdateUserName(uid, cid, newName)
+}
+
+func (s *classUserServiceImpl) GetUserClasses(uid uint) ([]dto.UserClassInfoDTO, error) {
+	return s.classUserRepo.GetUserClasses(uid)
 }
