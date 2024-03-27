5d4224e2c9befbbd7051fc56b78d368b7b7b76a9
devYuMinKim <gimyumin40@gmail.com>
2024-03-28 00:34:30 +0900

    🐛 fix: Restrict CORS to Allow Only Specific Origin
    This commit restricts the Cross-Origin Resource Sharing (CORS) settings to allow requests only from the specified origin (http://localhost:3000). Previously, the CORS settings were configured to allow requests from any origin. This change enhances security by limiting the accessibility of resources to a specific development environment.
    
    Related issue: #101

--- main.go
+++ main.go
@@ -125,10 +125,10 @@ func setupRouter(db *gorm.DB) *gin.Engine {
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, PATCH, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
