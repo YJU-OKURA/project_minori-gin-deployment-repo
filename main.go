9f6c86611dce67d137520372fd9c2c465c6791ac
devYuMinKim <gimyumin40@gmail.com>
2024-03-27 16:38:37 +0900

    - Previously, CORS settings only allowed requests from 'http://localhost:3000' - Updated to permit requests from all origins by changing the `Access-Control-Allow-Origin` header to '*' - This modification is expected to improve accessibility across various development and deployment environments

--- main.go
+++ main.go
@@ -125,7 +125,7 @@ func setupRouter(db *gorm.DB) *gin.Engine {
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
