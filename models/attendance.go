6102b60724ac67282ecd722ae6efc87130c81182
devYuMinKim <gimyumin40@gmail.com>
2024-06-16 15:36:45 +0900

    ♻️ refactor: Standardize AttendanceType and enum values
    - Introduced a new `AttendanceType` type in `models/attendance.go` to standardize the attendance status values.
    - Updated the `IsAttendance` field in the `Attendance` struct to use the `AttendanceType` type with standardized enum values 'ATTENDANCE', 'TARDY', and 'ABSENCE'.
    
    This change helps ensure consistency and prevents errors related to mismatched status strings.
    
    Related issue: #325

--- models/attendance.go
+++ models/attendance.go
@@ -1,11 +1,19 @@
 package models
 
+type AttendanceType string
+
+const (
+	AttendanceStatus AttendanceType = "ATTENDANCE"
+	TardyStatus      AttendanceType = "TARDY"
+	AbsenceStatus    AttendanceType = "ABSENCE"
+)
+
 type Attendance struct {
-	ID            uint          `gorm:"primaryKey;size:255;autoIncrement;"`
-	CID           uint          `gorm:"column:cid;not null"`                                                    // Class ID
-	UID           uint          `gorm:"column:uid;not null"`                                                    // User ID
-	CSID          uint          `gorm:"column:csid;not null"`                                                   // Class Schedule ID
-	IsAttendance  string        `gorm:"type:enum('Attendance', 'Tardy', 'Absence');default:'Absence';not null"` // 出席, 遅刻, 欠席
-	ClassUser     ClassUser     `gorm:"foreignKey:CID,UID"`
-	ClassSchedule ClassSchedule `gorm:"foreignKey:CSID"`
+	ID            uint           `gorm:"primaryKey;size:255;autoIncrement;"`
+	CID           uint           `gorm:"column:cid;not null"`                                                    // Class ID
+	UID           uint           `gorm:"column:uid;not null"`                                                    // User ID
+	CSID          uint           `gorm:"column:csid;not null"`                                                   // Class Schedule ID
+	IsAttendance  AttendanceType `gorm:"type:enum('ATTENDANCE', 'TARDY', 'ABSENCE');default:'ABSENCE';not null"` // 출席, 遅刻, 欠席
+	ClassUser     ClassUser      `gorm:"foreignKey:CID,UID"`
+	ClassSchedule ClassSchedule  `gorm:"foreignKey:CSID"`
 }
