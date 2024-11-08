1f04059654998d36d3b95366f0333e6f641d2db6
YuMin Kim <gimyumin40@gmail.com>
2024-11-08 11:15:27 +0900

    ♻️ refactor: Enhance attendance management system
    - Refactor attendance handling to use CSID for better tracking
    - Add attendance statistics functionality by CID and CSID
    - Update class schedule time fields naming convention
    - Modify API endpoints and update documentation
    - Preload related entities for detailed attendance information
    
    Related issue: #325

--- repositories/attendance_repository.go
+++ repositories/attendance_repository.go
@@ -5,61 +5,81 @@ import (
 	"gorm.io/gorm"
 )
 
-// AttendanceRepository インタフェース
 type AttendanceRepository interface {
 	CreateAttendance(attendance *models.Attendance) error
-	GetAttendanceByUIDAndCID(uid uint, cid uint) (*models.Attendance, error)
+	GetAttendanceByUIDAndCSID(uid uint, cid uint) (*models.Attendance, error)
 	GetAllAttendancesByCID(cid uint) ([]models.Attendance, error)
-	GetAttendanceByID(id string) ([]models.Attendance, error)
+	GetAllAttendancesByCSID(csid uint) ([]models.Attendance, error)
 	UpdateAttendance(attendance *models.Attendance) error
-	DeleteAttendance(id string) error
+	DeleteAttendance(id uint) error
+	GetAttendanceStatisticsByCID(cid uint) (map[models.AttendanceType]int, error)
+	GetAttendanceStatisticsByCSID(csid uint) (map[models.AttendanceType]int, error)
 }
 
-// attendanceConnection グループ掲示板リポジトリ
 type attendanceRepository struct {
 	db *gorm.DB
 }
 
-// NewAttendanceRepository グループ掲示板リポジトリを生成
 func NewAttendanceRepository(db *gorm.DB) AttendanceRepository {
 	return &attendanceRepository{db: db}
 }
 
-// CreateAttendance 出席情報を作成
 func (repo *attendanceRepository) CreateAttendance(attendance *models.Attendance) error {
 	return repo.db.Create(attendance).Error
 }
 
-// GetAttendanceByUIDAndCID UIDとCIDによって出席情報を取得
-func (repo *attendanceRepository) GetAttendanceByUIDAndCID(uid uint, cid uint) (*models.Attendance, error) {
+func (repo *attendanceRepository) GetAttendanceByUIDAndCSID(uid uint, cid uint) (*models.Attendance, error) {
 	var attendance models.Attendance
 	err := repo.db.Where("uid = ? AND cid = ?", uid, cid).First(&attendance).Error
 	return &attendance, err
 }
 
-// GetAllAttendancesByCID CIDによって全ての出席情報を取得
 func (repo *attendanceRepository) GetAllAttendancesByCID(cid uint) ([]models.Attendance, error) {
+	var attendances []models.Attendance
+	err := repo.db.Preload("ClassUser").Where("cid = ?", cid).Find(&attendances).Error
+	return attendances, err
+}
+
+func (repo *attendanceRepository) GetAllAttendancesByCSID(csid uint) ([]models.Attendance, error) {
+	var attendances []models.Attendance
+	err := repo.db.Preload("ClassUser").Where("csid = ?", csid).Find(&attendances).Error
+	return attendances, err
+}
+
+func (repo *attendanceRepository) UpdateAttendance(attendance *models.Attendance) error {
+	return repo.db.Save(attendance).Error
+}
+
+func (repo *attendanceRepository) DeleteAttendance(id uint) error {
+	return repo.db.Delete(&models.Attendance{}, id).Error
+}
+
+func (repo *attendanceRepository) GetAttendanceStatisticsByCID(cid uint) (map[models.AttendanceType]int, error) {
 	var attendances []models.Attendance
 	err := repo.db.Where("cid = ?", cid).Find(&attendances).Error
 	if err != nil {
 		return nil, err
 	}
-	return attendances, err
+
+	statistics := make(map[models.AttendanceType]int)
+	for _, attendance := range attendances {
+		statistics[attendance.IsAttendance]++
+	}
+
+	return statistics, nil
 }
 
-// GetAttendanceByID IDによって出席情報を取得
-func (repo *attendanceRepository) GetAttendanceByID(id string) ([]models.Attendance, error) {
+func (repo *attendanceRepository) GetAttendanceStatisticsByCSID(csid uint) (map[models.AttendanceType]int, error) {
 	var attendances []models.Attendance
-	err := repo.db.Preload("ClassUser").Preload("ClassUser.User").Preload("ClassUser.Class").Where("csid = ?", id).Find(&attendances).Error
-	return attendances, err
-}
+	err := repo.db.Where("csid = ?", csid).Find(&attendances).Error
+	if err != nil {
+		return nil, err
+	}
 
-// UpdateAttendance 出席情報を更新
-func (repo *attendanceRepository) UpdateAttendance(attendance *models.Attendance) error {
-	return repo.db.Save(attendance).Error
-}
+	statistics := make(map[models.AttendanceType]int)
+	for _, attendance := range attendances {
+		statistics[attendance.IsAttendance]++
+	}
 
-// DeleteAttendance 出席情報を削除
-func (repo *attendanceRepository) DeleteAttendance(id string) error {
-	return repo.db.Delete(&models.Attendance{}, id).Error
+	return statistics, nil
 }
